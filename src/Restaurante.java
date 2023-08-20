5d68e85e599d72f8f8ef9df46fb509ce64b61e6d
Lucas Abdo <lucarioales@gmail.com>
2023-08-20 13:14:54 -0300

    Objetos instanciados na classe executora

--- src/Restaurante.java
+++ src/Restaurante.java
@@ -2,6 +2,26 @@ import java.util.Scanner;
 
 public class Restaurante {
     public static void main(String[] args) throws Exception {
-        
+        Scanner scanner = new Scanner(System.in);
+        Cardapio cardapio = new Cardapio();
+        Cliente cliente = new Cliente();
+        Garcom garcom = new Garcom(0, 0, "Garçom", 1238.0f, "Dave", 18);
+        Cozinheiro cozinheiro = new Cozinheiro(0, "Cozinheiro", 1956.46f, "Lucas", 17);
+        Caixa caixa = new Caixa(0, "Caixa", 1616.24f, "Pedro", 18);
+
+
+
+        int opcao;
+        String nomeRestaurante = "Delícias do Ricardo";
+        String nomeCliente;
+
+        System.out.print("Olá, somos do Restaurante " + nomeRestaurante + ", qual é o seu nome? ");
+        nomeCliente = scanner.nextLine();
+        cliente.setNome(nomeCliente);
+
+        System.out.println(cliente.getNome() + ", aqui está um menu de ações.");
+        do {
+            break;
+        } while(true);
     }
 }
